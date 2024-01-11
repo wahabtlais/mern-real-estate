8be389d23d272bcdc5995f7ac908425a1f58c404
Wahab Tlais <wahabtlais@gmail.com>
2024-01-11 20:37:17 +0200

    Complete signup page ui and functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,81 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 const SignUp = () => {
-	return <div>SignUp</div>;
+	const [formData, setFormData] = useState({});
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value });
+	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			setLoading(true);
+			setError(false);
+			const res = await fetch("/api/auth/signup", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			setLoading(false);
+			if (data.success === false) {
+				setError(true);
+				return;
+			}
+			navigate("/sign-in");
+		} catch (error) {
+			setLoading(false);
+			setError(true);
+		}
+	};
+	return (
+		<div className="p-3 max-w-lg mx-auto">
+			<h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+			<form onSubmit={handleSubmit} className="flex flex-col gap-4">
+				<input
+					type="text"
+					placeholder="Username"
+					id="username"
+					className="bg-slate-200 p-3 rounded-lg"
+					onChange={handleChange}
+				/>
+				<input
+					type="email"
+					placeholder="Email"
+					id="email"
+					className="bg-slate-200 p-3 rounded-lg"
+					onChange={handleChange}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					id="password"
+					className="bg-slate-200 p-3 rounded-lg"
+					onChange={handleChange}
+				/>
+				<button
+					disabled={loading}
+					className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+				>
+					{loading ? "Loading..." : "Sign Up"}
+				</button>
+			</form>
+			<div className="flex text-sm p-1 mt-2">
+				<p>Have an account?</p>
+				<Link to="/sign-in">
+					<span className="text-blue-500 ml-1">SignIn</span>
+				</Link>
+			</div>
+			<p className="text-red-600 text-center py-2 font-semibold">
+				{error && "Failed to sign up."}
+			</p>
+		</div>
+	);
 };
 
 export default SignUp;
