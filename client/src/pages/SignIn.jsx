02c1085d6f3ee2de57c6dae81fdd09c22ca56b01
Wahab Tlais <wahabtlais@gmail.com>
2024-01-12 21:45:14 +0200

    Create signin page and add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,78 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+	signInStart,
+	signInSuccess,
+	signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 const SignIn = () => {
-	return <div>SignIn</div>;
+	const [formData, setFormData] = useState({});
+	const { loading, error } = useSelector((state) => state.user);
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value });
+	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			dispatch(signInStart());
+			const res = await fetch("/api/auth/signin", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (data.success === false) {
+				dispatch(signInFailure(data));
+				return;
+			}
+			dispatch(signInSuccess(data));
+			navigate("/");
+		} catch (error) {
+			dispatch(signInFailure(error));
+		}
+	};
+	return (
+		<div className="p-3 max-w-lg mx-auto">
+			<h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+			<form onSubmit={handleSubmit} className="flex flex-col gap-4">
+				<input
+					type="email"
+					placeholder="Email"
+					id="email"
+					className="bg-slate-200 p-3 rounded-lg"
+					onChange={handleChange}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					id="password"
+					className="bg-slate-200 p-3 rounded-lg"
+					onChange={handleChange}
+				/>
+				<button
+					disabled={loading}
+					className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+				>
+					{loading ? "Loading..." : "Sign In"}
+				</button>
+			</form>
+			<div className="flex text-sm p-1 mt-2">
+				<p>Don&apos;t have an account?</p>
+				<Link to="/sign-up">
+					<span className="text-blue-500 ml-1">Sign Up</span>
+				</Link>
+			</div>
+			<p className="text-red-600 text-center py-2 font-semibold">
+				{error ? error.message || "Something went wrong!" : ""}
+			</p>
+		</div>
+	);
 };
 
 export default SignIn;
