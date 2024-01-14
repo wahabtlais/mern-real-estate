ef9ad126945f834c030662cac16becf09bb560f3
Wahab Tlais <wahabtlais@gmail.com>
2024-01-14 20:33:56 +0200

    Add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -27,3 +27,17 @@ export const updateUser = async (req, res, next) => {
 		next(error);
 	}
 };
+
+export const deleteUser = async (req, res, next) => {
+	if (req.user.id !== req.params.id) {
+		return next(errorHandler(403, "You cannot delete another user's"));
+	}
+
+	try {
+		await User.findByIdAndDelete(req.params.id);
+		res.clearCookie("access_token");
+		res.status(200).json("User has been deleted successfully!");
+	} catch (error) {
+		next(error);
+	}
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { updateUser } from "../controllers/user.controller.js";
+import { deleteUser, updateUser } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/update/:id", verifyUser, updateUser);
+router.delete("/delete/:id", verifyUser, deleteUser);
 
 export default router;
