07dae6f8fe688c8cd7afb3af37787934875945bb
Wahab Tlais <wahabtlais@gmail.com>
2024-01-14 20:40:10 +0200

    Add signout user api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -80,3 +80,12 @@ export const google = async (req, res, next) => {
 		next(error);
 	}
 };
+
+export const signout = async (req, res, next) => {
+	try {
+		res.clearCookie("access_token");
+		res.status(200).json("User has been logged out successfully!");
+	} catch (error) {
+		next(error);
+	}
+};
