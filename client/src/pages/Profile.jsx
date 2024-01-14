55c2b55871796b5fc0c02c1bc4931fea262dfb16
Wahab Tlais <wahabtlais@gmail.com>
2024-01-14 19:50:33 +0200

    Create profile component ui with image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,186 @@
+import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import {
+	getDownloadURL,
+	getStorage,
+	ref,
+	uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import {
+	updateUserStart,
+	updateUserSuccess,
+	updateUserFailure,
+	deleteUserStart,
+	deleteUserSuccess,
+	deleteUserFailure,
+	signOut,
+} from "../redux/user/userSlice";
+
 const Profile = () => {
-	return <div>Profile</div>;
+	const fileRef = useRef(null);
+	const dispatch = useDispatch();
+	const [image, setImage] = useState(undefined);
+	const [imagePercent, setImagePercent] = useState(0);
+	const [imageError, setImageError] = useState(false);
+	const [formData, setFormData] = useState({});
+	const [updateSuccess, setUpdateSuccess] = useState(false);
+
+	const { currentUser, loading, error } = useSelector((state) => state.user);
+	const handleFileUpload = (image) => {
+		const storage = getStorage(app);
+		const fileName = new Date().getTime() + image.name;
+		const storageRef = ref(storage, fileName);
+		const uploadTask = uploadBytesResumable(storageRef, image);
+		uploadTask.on(
+			"state_changed",
+			(snapshot) => {
+				const progress =
+					(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+				setImagePercent(Math.round(progress));
+			},
+			() => {
+				setImageError(true);
+			},
+			() => {
+				getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+					setFormData({ ...formData, profilePicture: downloadURL });
+				});
+			}
+		);
+	};
+	useEffect(() => {
+		if (image) {
+			handleFileUpload(image);
+		}
+	}, [image]);
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value });
+	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			dispatch(updateUserStart());
+			const res = await fetch(`/api/user/update/${currentUser._id}`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (data.success === false) {
+				dispatch(updateUserFailure(data));
+				return;
+			}
+			dispatch(updateUserSuccess(data));
+			setUpdateSuccess(true);
+		} catch (error) {
+			dispatch(updateUserFailure(error));
+		}
+	};
+	const handleDeleteAccount = async () => {
+		try {
+			dispatch(deleteUserStart());
+			const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+				method: "DELETE",
+			});
+			const data = await res.json();
+			if (data.success === false) {
+				dispatch(deleteUserFailure(data));
+				return;
+			}
+			dispatch(deleteUserSuccess(data));
+		} catch (error) {
+			dispatch(deleteUserFailure(error));
+		}
+	};
+	const handleSignOut = async () => {
+		try {
+			await fetch("/api/auth/signout");
+			dispatch(signOut());
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<div className="p-3 max-w-lg mx-auto">
+			<h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+			<form onSubmit={handleSubmit} className="flex flex-col gap-4">
+				<input
+					type="file"
+					ref={fileRef}
+					hidden
+					accept="image/*"
+					onChange={(e) => setImage(e.target.files[0])}
+				/>
+				<img
+					src={formData.profilePicture || currentUser.profilePicture}
+					alt="profile"
+					className="h-24 w-24 self-center cursor-pointer rounded-full object-cover"
+					onClick={() => fileRef.current.click()}
+				/>
+				<p className="text-sm self-center">
+					{imageError ? (
+						<span className="text-red-600">
+							Error uploading image (File size be less than 2 MB)
+						</span>
+					) : imagePercent > 0 && imagePercent < 100 ? (
+						<span className="text-slate-700">{`Uploading: ${imagePercent}%`}</span>
+					) : imagePercent === 100 ? (
+						<span className="text-green-700">Image uploaded successfully</span>
+					) : null}
+				</p>
+				<input
+					defaultValue={currentUser.username}
+					type="text"
+					id="username"
+					placeholder="Username"
+					className="bg-slate-200 rounded-lg p-3"
+					onChange={handleChange}
+				/>
+				<input
+					defaultValue={currentUser.email}
+					type="email"
+					id="email"
+					placeholder="Email"
+					className="bg-slate-200 rounded-lg p-3"
+					onChange={handleChange}
+				/>
+				<input
+					type="password"
+					id="password"
+					placeholder="Change Password"
+					className="bg-slate-200 rounded-lg p-3"
+					onChange={handleChange}
+				/>
+				<button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+					{loading ? "Loading..." : "Update"}
+				</button>
+			</form>
+			<div className="flex justify-between mt-4">
+				<span
+					onClick={handleDeleteAccount}
+					className="text-red-600 cursor-pointer font-semibold"
+				>
+					Delete Account
+				</span>
+				<span
+					onClick={handleSignOut}
+					className="text-red-600 cursor-pointer font-semibold"
+				>
+					Sign out
+				</span>
+			</div>
+			<div className="text-center">
+				<p className="text-red-700 mt-3">{error && "Something went wrong!"}</p>
+				<p className="text-green-700 mt-3">
+					{updateSuccess && "Profile updated successfully!"}
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
