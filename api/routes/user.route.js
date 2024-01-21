81b9694564a75f7425545f3bbe1c45fd621b605d
Wahab Tlais <wahabtlais@gmail.com>
2024-01-21 20:41:29 +0200

    Create get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { deleteUser, updateUser } from "../controllers/user.controller.js";
+import {
+	deleteUser,
+	updateUser,
+	getUserListings,
+} from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/update/:id", verifyUser, updateUser);
 router.delete("/delete/:id", verifyUser, deleteUser);
+router.get("/listings/:id", verifyUser, getUserListings);
 
 export default router;
