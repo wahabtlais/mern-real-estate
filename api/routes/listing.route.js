ca64fc09e1b296ed65faf300577d40e6974df646
Wahab Tlais <wahabtlais@gmail.com>
2024-02-16 16:41:43 +0200

    Create search API route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -4,6 +4,7 @@ import {
 	deleteListing,
 	updateListing,
 	getListing,
+	getListings,
 } from "../controllers/listing.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
@@ -13,5 +14,6 @@ router.post("/create", verifyUser, createListing);
 router.delete("/delete/:id", verifyUser, deleteListing);
 router.post("/update/:id", verifyUser, updateListing);
 router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
