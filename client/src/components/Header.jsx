351a00558393579b732a4c07c7f6d611ff913423
Wahab Tlais <wahabtlais@gmail.com>
2024-02-16 17:22:08 +0200

    Complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,29 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const Header = () => {
 	const { currentUser } = useSelector((state) => state.user);
+	const [searchTerm, setSearchTerm] = useState("");
+	const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const urlParams = new URLSearchParams(window.location.search);
+		urlParams.set("searchTerm", searchTerm);
+		const searchQuery = urlParams.toString();
+		navigate(`/search?${searchQuery}`);
+	};
+
+	useEffect(() => {
+		const urlParams = new URLSearchParams(location.search);
+		const searchTermFromUrl = urlParams.get("searchTerm");
+		if (searchTermFromUrl) {
+			setSearchTerm(searchTermFromUrl);
+		}
+	}, [location.search]);
+
 	return (
 		<header className="bg-slate-200 shadow-md">
 			<div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -13,15 +33,22 @@ const Header = () => {
 						<span className="text-slate-700">Abode</span>
 					</h1>
 				</Link>
-				<form className="bg-slate-100 p-2 rounded-lg flex items-center">
+				<form
+					onSubmit={handleSubmit}
+					className="bg-slate-100 p-2 rounded-lg flex items-center"
+				>
 					<input
 						type="search"
 						name="search"
 						id="search"
 						placeholder="Search..."
 						className="bg-transparent focus:outline-none w-24 sm:w-64 p-1 text-slate-700"
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
 					/>
-					<FaSearch className="text-slate-700" />
+					<button>
+						<FaSearch className="text-slate-700" />
+					</button>
 				</form>
 				<ul className="flex gap-4 font-semibold text-slate-700 cursor-pointer">
 					<Link to="/">
