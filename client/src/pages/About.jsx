7dc98df458f26ad979271aff681c4441893d8b55
Wahab Tlais <wahabtlais@gmail.com>
2024-02-21 13:11:21 +0200

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,23 @@
 const About = () => {
-	return <div>About</div>;
+	return (
+		<div className="py-20 px-4 max-w-6xl mx-auto">
+			<h1 className="text-3xl font-bold mb-4 text-slate-800">
+				About Urban Abode
+			</h1>
+			<p className="text-slate-700">
+				Welcome to our real estate platform, where dreams find their home. With
+				a passion for connecting people with their perfect properties, we strive
+				to make the journey of buying, selling, or renting a seamless and
+				enjoyable experience. Our dedicated team of experts is committed to
+				providing personalized assistance every step of the way, ensuring that
+				your unique needs are met with professionalism and integrity. Whether
+				you&apos;re searching for your dream home, an investment opportunity, or
+				a commercial space to grow your business, let us be your trusted guide
+				in navigating the exciting world of real estate. Welcome to a place
+				where your vision meets reality.
+			</p>
+		</div>
+	);
 };
 
 export default About;
